230ac793271ddfc033e33432a1c95941da804e55
elgs <elgs1980@hotmail.com>
2014-06-23 10:59:55 +0800

    initial commits

---
+++ yotta.js
@@ -0,0 +1,20 @@
+/**
+ * Created by elgs on 6/23/14.
+ */
+
+(function () {
+    "use strict";
+
+    var opened = false;
+    var open = function () {
+    };
+
+    var close = function () {
+    };
+
+    var put = function (key, value) {
+    };
+
+    var get = function (key) {
+    };
+})();
